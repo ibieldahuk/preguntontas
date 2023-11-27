77647e89416f868c259aa4b15d09041dc7037a85
Fernandez Guadalupe <guadapfernandez@gmail.com>
2023-11-27 13:29:11 -0300

    timer

--- public/js/tiempo_partida.js
+++ public/js/tiempo_partida.js
@@ -1,23 +1,20 @@
-function move() {
-    var elem = document.getElementById("myBar");
-    var width = 100; // Ancho inicial
-    var duration = 30000; // Duración en milisegundos (5 segundos)
-    var frameRate = 10; // Intervalo de actualización (ms)
-    var frames = duration / frameRate;
-    var step = width / frames;
-    var i = 0;
+let timer = 15;
+let mostrarTiempo = document.getElementById("time");
+let tiempoRegresivoId = null;
 
-    function frame() {
-        if (i < frames) {
-            i++;
-            width -= step;
-            elem.style.width = width + "%";
-            var remainingTime = (frames - i) * frameRate / 1000;
-        } else {
-            clearInterval(id);
+contarTiempo();
+
+function contarTiempo(){
+    tiempoRegresivoId = setInterval(()=>{
+        timer--;
+        if(timer < 10){
+            mostrarTiempo.innerHTML = `00:0${timer}`;
+        }else{
+            mostrarTiempo.innerHTML = `00:${timer}`;
         }
-    }
-    var id = setInterval(frame, frameRate);
+        if(timer == 0){
+            clearInterval(tiempoRegresivoId);
+        }
+    }, 1000);
 }
 
-move();
